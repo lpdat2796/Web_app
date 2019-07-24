c863dbea37497f2f28a715a2ee8afacf2a630bac
lpdat2796 <lpdat2796@gmail.com>
2019-07-24 16:20:15 +0700

    update

--- db/migrate/20190718025151_create_books.rb
+++ db/migrate/20190718025151_create_books.rb
@@ -5,7 +5,7 @@ class CreateBooks < ActiveRecord::Migration[5.1]
       t.string    :author
       t.string    :title
       t.string    :publisher
-      t.date      :year
+      t.datetime  :year
       t.integer   :page             
       t.string    :language
       t.string    :size
