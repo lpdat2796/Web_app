ed0ad3ba27bb004d2458867883feb58cab843b48
lpdat2796 <lpdat2796@gmail.com>
2019-07-25 09:44:14 +0700

    update logic for login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   
-    protected
-  
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) do |u|
-        u.permit(:email, :username, :password, :password_confirmation)
-    
-      end
-    end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username, :password, :password_confirmation])
   end
+  
 end
