1a64fd2d9ce3dbdfe843c82e63baf717860fed1d
lpdat2796 <lpdat2796@gmail.com>
2019-08-05 10:40:36 +0700

    fix bug

--- db/migrate/20190724060918_add_file_to_books.rb
+++ db/migrate/20190724060918_add_file_to_books.rb
@@ -3,5 +3,6 @@
 class AddFileToBooks < ActiveRecord::Migration[5.1]
   def change
     add_column :books, :file, :string
+    
   end
 end
--- db/migrate/20190724091116_add_confirm_column.rb
+++ db/migrate/20190724091116_add_confirm_column.rb
@@ -6,7 +6,6 @@ class AddConfirmColumn < ActiveRecord::Migration[5.1]
     add_column :users, :confirmed_at, :datetime
     add_column :users, :confirmation_sent_at, :datetime
     add_column :users, :unconfirmed_email, :string
-
     add_index :users, :confirmation_token, unique: true
   end
 end
