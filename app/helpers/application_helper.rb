0cef67983ce4a918b7bdf2e4efd4f5e61b4a6293
lpdat2796 <lpdat2796@gmail.com>
2019-08-15 17:32:04 +0700

    refactory code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@
 
 module ApplicationHelper
   def show_left_menu?
-    !request.path.in? [new_user_session_path,login_path, register_path, password_reset_path, new_user_confirmation_path, user_confirmation_path]
+    !devise_controller?
   end
 end
